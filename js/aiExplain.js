cfe00c95e3d97493c85a271a9cb4bbe9d8130c7f
ItzWildaYT <hussain.hashim1118@gmail.com>
2025-10-06 14:09:17 -0400

    Delete js directory

--- js/aiExplain.js
+++
@@ -1,8 +0,0 @@
-const explainEndpoint = '/.netlify/functions/explain'
-export async function explainWithServer(prompt){
-  const res = await fetch(explainEndpoint, {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({prompt})})
-  if(!res.ok) throw new Error('explain failed')
-  const data = await res.json()
-  return data
-}
-export default explainWithServer
