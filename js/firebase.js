072f26a44e6241235adc9f102f5e4ca42ab9ce1b
ItzWildaYT <hussain.hashim1118@gmail.com>
2025-10-06 14:53:29 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.3.0/firebas
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js'
 import { getFirestore, collection, addDoc, query, orderBy, getDocs, serverTimestamp, doc, updateDoc, limit } from 'https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js'
 const firebaseConfig = {
-  apiKey: 'FIREBASE_API_KEY',
-  authDomain: 'FIREBASE_AUTH_DOMAIN',
-  projectId: 'FIREBASE_PROJECT_ID',
-  storageBucket: 'FIREBASE_STORAGE_BUCKET',
-  messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
-  appId: 'FIREBASE_APP_ID',
-  measurementId: 'FIREBASE_MEASUREMENT_ID'
-}
+  apiKey: "AIzaSyAsfxkRiVrenBmfDhwSpKWvgsgJ4f1w6Vs",
+  authDomain: "coderr-a3820.firebaseapp.com",
+  projectId: "coderr-a3820",
+  storageBucket: "coderr-a3820.firebasestorage.app",
+  messagingSenderId: "1066035377317",
+  appId: "1:1066035377317:web:5065cf5d96141307d8c94c",
+  measurementId: "G-HF98H4CX6C"
+};
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore(app)
