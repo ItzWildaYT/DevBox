f26afac4725934c16eba2105f7152265ebfa734e
ItzWildaYT <hussain.hashim1118@gmail.com>
2025-10-06 15:37:58 -0400

    Update aiExplain.js

--- js/aiExplain.js
+++ js/aiExplain.js
@@ -1,5 +1,5 @@
 export default async function explainWithServer(code) {
-  const response = await fetch('/.netlify/functions/explain.js', {
+  const response = await fetch('/.netlify/functions/explain', {
     method: 'POST',
     body: JSON.stringify({ code }),
     headers: { 'Content-Type': 'application/json' },
