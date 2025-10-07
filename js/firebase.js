c76e221149d6a18cd1773ebe1bad1a78270c873f
ItzWildaYT <hussain.hashim1118@gmail.com>
2025-10-06 22:29:12 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,23 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js'
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js'
-import { getFirestore, collection, addDoc, query, orderBy, getDocs, serverTimestamp, doc, updateDoc, limit } from 'https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  where,
+  limit,
+  serverTimestamp
+} from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js'
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut
+} from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js'
+
 const firebaseConfig = {
   apiKey: "AIzaSyAsfxkRiVrenBmfDhwSpKWvgsgJ4f1w6Vs",
   authDomain: "coderr-a3820.firebaseapp.com",
@@ -10,7 +27,24 @@ const firebaseConfig = {
   appId: "1:1066035377317:web:5065cf5d96141307d8c94c",
   measurementId: "G-HF98H4CX6C"
 };
+
 const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
 const db = getFirestore(app)
-export { auth, db, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut, collection, addDoc, query, orderBy, getDocs, serverTimestamp, doc, updateDoc, limit }
+const auth = getAuth(app)
+
+export {
+  auth,
+  db,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  where,
+  limit,
+  serverTimestamp
+}
